acfce0d9f8d9084906ec4122a9639925b9583548
Priyanshi Jajoo <priyanshi.jajoo@sjsu.edu>
2019-10-30 14:13:01 -0700

    Isomorphic Strings

---
+++ src/ReverseString/IsomorphicString.java
@@ -0,0 +1,38 @@
+package ReverseString;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.Set;
+
+public class IsomorphicString {
+    public boolean isIsomorphicStr(String s, String t){
+        if(s.length()!=t.length() || s==null || t==null){
+            return false;
+        }
+            char[] s1 = s.toCharArray();
+            char[] t1 = t.toCharArray();
+            HashMap<Character, Character> hmap = new HashMap<Character, Character>();
+            for (int i = 0; i < s1.length; i++) {
+                if (hmap.containsKey(s1[i])) {
+                    if (hmap.get(s1[i]) == t1[i]) {
+                        continue;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    if(hmap.containsValue(t1[i])){
+                        return false;
+                    }
+                    hmap.put(s1[i], t1[i]);
+                }
+
+            }
+            return true;
+    }
+        public static void main(String[] args){
+            IsomorphicString str= new IsomorphicString();
+            System.out.println(str.isIsomorphicStr("eggf", "adda"));
+
+        }
+}
+// Time complexity: O(n) and Space complexity: O(1)
