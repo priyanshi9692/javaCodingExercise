0a0c6035fe2efa24b89c8703d280161432daa78e
Priyanshi Jajoo <priyanshi.jajoo@sjsu.edu>
2020-01-26 17:00:52 -0800

    Classic Needle and Haystack Problem; Implementation of strStr()

--- src/StringProblems/StrStr.java
+++ src/StringProblems/StrStr.java
@@ -1,4 +1,32 @@
 package StringProblems;
 
 public class StrStr {
+    public int strStr(String haystack, String needle){
+        if(haystack==null || needle == null){
+            return 0;
+        }
+        if(needle.length()==0){
+            return 0;
+        }
+        for(int i=0; i< haystack.length(); i++){
+            if(i+needle.length()>haystack.length()){
+                return -1;
+            }
+            int k=i;
+            for(int j=0 ; j<needle.length(); j++){
+                if(needle.charAt(j)==haystack.charAt(k)){
+                    if(j==needle.length()-1)
+                        return i;
+                    k++;
+                }else{
+                    break;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String [] args){
+        StrStr obj= new StrStr();
+        System.out.println(obj.strStr("goodteacher", "teach"));
+    }
 }
