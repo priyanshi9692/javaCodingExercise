de9f9953afe7bab01d75ff79a23f88e4e3bec165
Priyanshi Jajoo <priyanshi.jajoo@sjsu.edu>
2020-01-26 16:58:58 -0800

    Remove every instance of an element from an array

--- src/NumericProblems/RemoveElement.java
+++ src/NumericProblems/RemoveElement.java
@@ -1,4 +1,23 @@
 package NumericProblems;
 
 public class RemoveElement {
+    public int removeElement(int [] nums, int val){
+        int j=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=val){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+    public static void main(String [] args){
+        RemoveElement obj= new RemoveElement();
+        int [] arr= {1};
+        int value= 5;
+        int n= obj.removeElement(arr, value);
+        for(int i =0; i<n; i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
 }
