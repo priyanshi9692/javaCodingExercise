a78e4fbf6660f33ba3a5c2368c844000519281bc
Priyanshi Jajoo <priyanshi.jajoo@sjsu.edu>
2020-06-03 15:38:55 -0700

    LeetCode 206. Reverse a singly linked list

---
+++ src/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,38 @@
+package LinkedList;
+/*
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+ */
+public class ReverseLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode reverseList(ListNode head){
+        ListNode prev = null;
+        while(head!=null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args){
+        ReverseLinkedList obj = new ReverseLinkedList();
+        ListNode head= new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        head.next=second;
+        second.next=third;
+        third.next=null;
+
+        System.out.println(obj.reverseList(head));
+    }
+}
