f28fa26147b4adec391745563bf7cf8e4837b164
Priyanshi Jajoo <priyanshi.jajoo@sjsu.edu>
2020-06-03 18:39:18 -0700

    LeetCode Problem 141. Linked List Cycle

--- src/LinkedList/LinkedListCycle.java
+++ src/LinkedList/LinkedListCycle.java
@@ -1,4 +1,54 @@
 package LinkedList;
+/*
+141. Linked List Cycle
+Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list,
+we use an integer pos which represents the position (0-indexed) in the
+linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+ */
 
 public class LinkedListCycle {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public boolean isCycle(ListNode head){
+        if(head == null || head.next == null){
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        LinkedListCycle obj = new LinkedListCycle();
+        ListNode head = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+        ListNode fifth = new ListNode(5);
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next =null;
+        System.out.println(obj.isCycle(head));
+    }
 }
